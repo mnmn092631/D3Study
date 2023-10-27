2b84e8685b9967af233b0adfb0aea4fa636da66d
juhee kang <mnmn092631@gmail.com>
2023-10-28 02:40:48 +0900

    Map - path key 설정

--- src/components/Map/Marks.tsx
+++ src/components/Map/Marks.tsx
@@ -18,8 +18,8 @@ export const Marks = ({
 		<g>
 			<path d={path({ type: "Sphere" }) || ""} fill="#ECECEC" />
 			<path d={path(graticule()) || ""} fill="none" stroke="#DADADA" />
-			{land.features.map((feature) => (
-				<path d={path(feature) || ""} fill="#137B80" />
+			{land.features.map((feature, i) => (
+				<path key={i} d={path(feature) || ""} fill="#137B80" />
 			))}
 			<path d={path(interiors) || ""} fill="none" stroke="#339498" />
 		</g>
